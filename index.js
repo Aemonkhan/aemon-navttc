bc03f5028fd09da9cd3c25d297b0ed2a200a93e2
Aemonkhan <ameonkhan789@gmail.com>
2020-11-06 13:03:59 +0500

    work

---
+++ index.js
@@ -0,0 +1,21 @@
+function displayDate(){
+    var date = new Date();
+    console.log (`${date}`)
+}
+var num =0;
+function count(){
+    num++;
+    console.log(`you have clicked ${num} times`)
+}
+function doo(){
+var x = document.getElementById("aa");
+x.innerHTML='my name';
+}
+function does(){
+    var x = document.getElementById("para");
+    if(x.innerHTML=="my dummy text"){
+     x.innerHTML ='new text';
+    }
+
+
+}
